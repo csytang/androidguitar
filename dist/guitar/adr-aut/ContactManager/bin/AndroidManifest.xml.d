6d0f84c124c371446767356975b3de88dfabfb91
lovetostrike <lovetostrike@email.com>
2013-06-02 13:29:02 -0400

    cleaning up mess

--- dist/guitar/adr-aut/ContactManager/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/lovetostrike/Workspace/Programming/Java/softwareEngineering/androidguitar/dist/guitar/adr-aut/ContactManager/bin/AndroidManifest.xml : \
-/home/lovetostrike/Workspace/Programming/Java/softwareEngineering/androidguitar/dist/guitar/adr-aut/ContactManager/AndroidManifest.xml \
--- dist/guitar/adr-aut/ContactManager/bin/ContactManager-instrumented-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/home/lovetostrike/Workspace/Programming/Java/softwareEngineering/androidguitar/dist/guitar/adr-aut/ContactManager/bin/ContactManager-instrumented-unaligned.apk : \
-/home/lovetostrike/Workspace/Programming/Java/softwareEngineering/androidguitar/dist/guitar/adr-aut/ContactManager/bin/ContactManager.ap_ \
-/home/lovetostrike/Workspace/Programming/Java/softwareEngineering/androidguitar/dist/guitar/adr-aut/ContactManager/bin/classes.dex \
-/home/lovetostrike/android-sdk-linux/tools/support/annotations.jar \
-/home/lovetostrike/android-sdk-linux/tools/lib/emma_device.jar \
